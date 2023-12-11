aee2065b79ff139ee2150eef54a3f1bcf3ff4beb
almoshqy <147120113+almoshqy@users.noreply.github.com>
2023-12-12 00:45:32 +0500

    Update brightness_offset.py

--- brightness_offset.py
+++ brightness_offset.py
@@ -6,7 +6,7 @@ input_path = sys.argv[1]
 output_path = sys.argv[2]
 
 img = cv2.imread(input_path)
-brightness_offset = 50  # Adjust this value to change brightness
+brightness_offset = 50  
 
 result = cv2.add(img, np.array([brightness_offset], dtype='uint8'))
 cv2.imwrite(output_path, result)
